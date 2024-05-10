b87681fed2fa3fc98e209d1409ea3e25e1e6613e
afhorton <106110920+afhorton@users.noreply.github.com>
2024-05-10 16:07:59 -0600

    Started Sign-up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # GET '/about'
   get 'about', to: 'about#index'
 
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
   root to: 'main#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
