1c61dc42cf734153469cc68b483193d0c8f5ec5e
afhorton <106110920+afhorton@users.noreply.github.com>
2024-05-11 18:33:53 -0600

    Created Sign_in form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,8 +9,7 @@ class RegistrationsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Successfully created account"
     else
-      render :new
-      # , status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
