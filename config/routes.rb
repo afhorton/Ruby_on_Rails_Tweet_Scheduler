b5ffc985d3224e988d6f83d17ec43050f3f9b483
afhorton <106110920+afhorton@users.noreply.github.com>
2024-05-15 13:51:50 -0600

    Created Twitter Accounts Page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
 
   get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
 
+  resources :twitter_accounts
+  # get "twitter_accounts/:id"
+  # delete "twitter_accounts/:id"
+
   root to: 'main#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
